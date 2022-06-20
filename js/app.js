4de6bf98c772615f1fc666e1595983007185c51b
David Sonis <92735576+DavidSonis@users.noreply.github.com>
2022-06-20 11:21:19 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// CODE EXPLAINED channel
+
 
 // Select the Elements
 const clear = document.querySelector(".clear");
@@ -6,7 +6,7 @@ const dateElement = document.getElementById("date");
 const list = document.getElementById("list");
 const input = document.getElementById("input");
 
-// Classes names
+// Clas names
 const CHECK = "fa-check-circle";
 const UNCHECK = "fa-circle-thin";
 const LINE_THROUGH = "lineThrough";
@@ -14,10 +14,10 @@ const LINE_THROUGH = "lineThrough";
 // Variables 
 let LIST, id;
 
-// get item from localstorage
+
 let data = localStorage.getItem("TODO");
 
-// check if data is not empty
+
 if(data){
     LIST = JSON.parse(data);
     id = LIST.length; // set the id to the last one in the list
@@ -47,7 +47,7 @@ const today = new Date();
 
 dateElement.innerHTML = today.toLocaleDateString("en-US", options);
 
-// add to do function
+//  to do function
 
 function addToDo(toDo, id, done, trash){
 
@@ -84,7 +84,7 @@ document.addEventListener("keyup",function(even){
                 trash : false
             });
 
-            // add item to localstorage ( this code must be added where the LIST array is updated)
+            // add item to local storage ( this code must be added where the LIST array is updated)
             localStorage.setItem("TODO", JSON.stringify(LIST));
 
             id++;
@@ -94,7 +94,7 @@ document.addEventListener("keyup",function(even){
 });
 
 
-// complete to do
+// completes to do
 function completeToDo(element){
     element.classList.toggle(CHECK);
     element.classList.toggle(UNCHECK);
@@ -103,14 +103,14 @@ function completeToDo(element){
     LIST[element.id].done = LIST[element.id].done ? false : true;
 }
 
-// remove to do
+// removes to do
 function removeToDo(element){
     element.parentNode.parentNode.removeChild(element.parentNode);
 
     LIST[element.id].trash = true;
 }
 
-// target the items created dynamically
+// targets the items created dynamically
 
 list.addEventListener("click", function(event){
     const element = event.target; // return the clicked element inside list
@@ -122,6 +122,6 @@ list.addEventListener("click", function(event){
         removeToDo(element);
     }
 
-    // add item to localstorage ( this code must be added where the LIST array is updated)
+    // add item to local storage ( this code must be added where the LIST array is updated)
     localStorage.setItem("TODO", JSON.stringify(LIST));
 });
